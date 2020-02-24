a4106323d13b50e6ef1b04b902ad144d57041d54
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-02-24 19:19:48 -0300

    Deletando e atualizando Jedi

--- src/main/java/com/example/springwebmvc/controller/JediController.java
+++ src/main/java/com/example/springwebmvc/controller/JediController.java
@@ -4,14 +4,14 @@ import com.example.springwebmvc.model.Jedi;
 import com.example.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class JediController {
@@ -37,9 +37,28 @@ public class JediController {
         }
 
         repository.save(jedi);
-        redirectAttributes.addFlashAttribute("message", "Jedi cadastrado com sucesso.");
+        redirectAttributes.addFlashAttribute("message", "Jedi successfully saved.");
 
         return "redirect:jedi";
+    }
+
+    @GetMapping("/jedi/{id}/delete")
+    public String deleteJedi (@PathVariable("id") final Long id, RedirectAttributes redirect) {
+        final Optional<Jedi> jedi = repository.findById(id);
+        repository.delete(jedi.get());
+
+        redirect.addFlashAttribute("message", "Jedi successfully deleted.");
+
+        return "redirect:/jedi";
+    }
+
+    @GetMapping("/jedi/{id}/update")
+    public String updateJedi (@PathVariable("id") final Long id, Model model) {
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        model.addAttribute("jedi", jedi.get());
+
+        return "edit-jedi";
     }
 
     @GetMapping("/new-jedi")
@@ -47,6 +66,15 @@ public class JediController {
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("new-jedi");
         modelAndView.addObject("jedi", new Jedi());
+        return modelAndView;
+    }
+
+    @GetMapping("/search")
+    public ModelAndView jediSearch (@RequestParam(value = "name") final String name) {
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+        modelAndView.addObject("allJedi", repository.findByNameContainingIgnoreCase(name));
+
         return modelAndView;
     }
 }
